7ce5f84c7839e9c7b42f74b00dfe5ec42a8b469a
Mohsin Siddiqui <82157778+mohsin-52715@users.noreply.github.com>
2022-12-21 12:58:14 +0530

    Update ValidateEnvironment.cmake

--- CMake/ValidateEnvironment.cmake
+++ CMake/ValidateEnvironment.cmake
@@ -216,7 +216,6 @@ if(CMAKE_HOST_SYSTEM_NAME STREQUAL Linux)
    check_linux_package("pulseaudio")
    check_linux_package("libdrm-dev")
    check_linux_package("libssl-dev")
-   check_linux_package("qemu-kvm")
    check_linux_package("qemu-utils")
    check_linux_package("libgtk-3-dev")
    
