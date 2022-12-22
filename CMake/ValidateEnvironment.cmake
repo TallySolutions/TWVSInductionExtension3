9ab7c03a05adef5da3695c12832b0e4f4b8fe8f2
karan-dixit-52886 <104002916+karan-dixit-52886@users.noreply.github.com>
2022-12-22 21:08:15 +0530

    Update ValidateEnvironment.cmake

--- CMake/ValidateEnvironment.cmake
+++ CMake/ValidateEnvironment.cmake
@@ -244,7 +244,7 @@ endif()
 macro(check_and_fix_homebrew_git_ownership)
     message(NOTICE "Verifying git HomeBrew Git Ownership  ...")
     execute_process(
-        COMMAND  eval /"$(/opt/homebrew/bin/brew shellenv)/"
+        COMMAND  eval $(/opt/homebrew/bin/brew shellenv)
         RESULT_VARIABLE retCode
         OUTPUT_VARIABLE out
         ERROR_VARIABLE error
